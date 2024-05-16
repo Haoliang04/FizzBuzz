a4d7f6eaf84ce9d8aa0b55848ff8cf8d6122602e
Haoliang Zhao <haoliang.zhao@mail.utoronto.ca>
2024-05-16 18:25:58 -0400

    Finish Task 3.3

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+//        FizzBuzz.doFizzBuzz(5);
+        // String[] a array of String
+        String[] str = {""};
+        FizzBuzz.main(str);
+    }
+}
